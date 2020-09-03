95b0d98d09c089feafdbb7492c20a5535edfe27c
robertobirijnr <robertobirijnr@hotmail.com>
2020-09-03 01:02:11 +0000

    jasjak

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
-let publicPath = process.env.NODE_ENV === 'production' ? 'recruitment-app/' : '/';
+let publicPath = process.env.NODE_ENV === 'production' ? './' : '/';
 
 module.exports = {
   configureWebpack: {
